d9192162249dc1f8cbf48b06ce38b89204e6ab06
cynraw <cynthiaronoh24@gmail.com>
2024-05-27 14:48:46 +0300

    Select statements in mysql

---
+++ bootcamp/select_mysql.sql
@@ -0,0 +1,13 @@
+SELECT * 
+FROM parks_and_recreation.employee_salary;
+
+SELECT first_name, 
+last_name,
+birth_date,
+age,
+age+15
+FROM employee_demographics;
+
+SELECT distinct gender
+FROM employee_demographics;
+
