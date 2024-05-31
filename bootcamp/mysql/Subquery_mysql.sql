c28ce81838bfd445417c1b8bfab55fee70ca5ecc
cynraw <cynthiaronoh24@gmail.com>
2024-05-31 10:28:08 +0300

    Subqueries in mysql

---
+++ bootcamp/mysql/Subquery_mysql.sql
@@ -0,0 +1,36 @@
+-- mysql subqueries
+-- subquery in where clause
+SELECT *
+FROM employee_demographics
+WHERE employee_id IN 
+				( SELECT employee_id
+					FROM employee_salary
+                    WHERE dept_id = 1)
+;
+
+-- Subquery in the select statements
+SELECT first_name, salary,
+	(SELECT AVG(salary)
+	FROM employee_salary)
+FROM employee_salary
+;
+
+-- Subquery in the FROM statement
+SELECT AVG(max_age)
+FROM
+(SELECT gender,
+AVG(age) AS avg_age,
+MAX(age) AS max_age,
+MIN(age) AS min_age,
+COUNT(age) 
+FROM employee_demographics
+GROUP BY  gender) AS agg_table
+;
+
+SELECT gender,
+AVG(age) AS avg_age,
+MAX(age) AS max_age,
+MIN(age) AS min_age,
+COUNT(age) 
+FROM employee_demographics
+GROUP BY  gender;
